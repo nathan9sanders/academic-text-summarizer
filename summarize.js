a68465b9aae043cb150e4da1f69bf446fbd1d889
Nathan Sanders <nathan9sanders@gmail.com>
2019-12-14 15:00:38 -0800

    js bugfix attempt

--- summarize.js
+++ summarize.js
@@ -1,22 +1,24 @@
-$('#submit-text').click(function() {
-  var url = "https://processintegrator.herokuapp.com/"; // send the form data here.
+$(document).ready(function() {
+  $("#submit-text").click(function() {
+    var url = "https://processintegrator.herokuapp.com/"; // send the form data here.
 
-  var article = {
-    "text": $("select#text-input").val()
-  };
-  console.log(article["text"]);
+    var article = {
+      "text": $("select#text-input").val()
+    };
+    console.log(article["text"]);
 
-  article = JSON.stringify(process_data);
+    article = JSON.stringify(process_data);
 
-  $.ajax({
-    type: "POST",
-    url: url,
-    data: article,
-    success: function (data) {
-      console.log(data);
-    },
-    error: function(error) {
-      console.log(error);
-    }
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: article,
+      success: function (data) {
+        console.log(data);
+      },
+      error: function(error) {
+        console.log(error);
+      }
+    });
   });
 });
