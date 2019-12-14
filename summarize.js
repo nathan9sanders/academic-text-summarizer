10d506e1a5834d850bda335df5c1b96ce5d73eb0
Nathan Sanders <nathan9sanders@gmail.com>
2019-12-14 14:54:19 -0800

    set proper function trigger for send

--- summarize.js
+++ summarize.js
@@ -1,10 +1,10 @@
-$(document).ready(function() {
+$('#submit-text').click(function() {
   var url = "https://processintegrator.herokuapp.com/"; // send the form data here.
 
   var article = {
     "text": $("select#text-input").val()
   };
-  console.log(article);
+  console.log(article["text"]);
 
   article = JSON.stringify(process_data);
 
